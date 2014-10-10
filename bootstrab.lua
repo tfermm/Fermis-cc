111778f8e221ca06309b79b1f62ac9e0a2057dcf
tfermm <tfermm@gmail.com>
2014-10-10 16:57:28 -0400

    Updated links to point to the right place

--- bootstrab.lua
+++ bootstrab.lua
@@ -1,7 +1,7 @@
 
 fs.makeDir("/fermis-cc")
 bootstrap = fs.open("/fermis-cc/bootstrap", "w")
-bootstrapConnection = http.get("http://fermis.me/fermis-cc/bootstrap.lua")
+bootstrapConnection = http.get("https://raw.githubusercontent.com/tfermm/Fermis-cc/master/bootstrab.lua")
 
 assert(bootstrap, "Unable to save installer to disk! Please make sure your in-game computer has space available and try again!")
 assert(bootstrapConnection, "Unable to download installer components! Is your internet working? See if you can access https://raw.github.com/damien/cc-scripts/master/bootstrap.lua")
@@ -11,7 +11,7 @@ bootstrapConnection.close()
 bootstrap.close()
 
 bootstrap = fs.open("/fermis-cc/bootstrap", "w")
-bootstrapConnection = http.get("http://fermis.me/fermis-cc/test.lua")
+bootstrapConnection = http.get("https://raw.githubusercontent.com/tfermm/Fermis-cc/master/harvester.lua")
 
 bootstrap.write(bootstrapConnection.readAll())
 bootstrapConnection.close()
